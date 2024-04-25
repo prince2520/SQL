2140ea16ff7c7ace36bfffb744878d07d3c15b20
Prince Kumar <prince2520p@gmail.com>
2024-04-25 19:12:50 +0530

    refactor: create database and table basics

---
+++ CreateDatabaseTable/create_database_table.sql
@@ -0,0 +1,33 @@
+--show databases
+/* SHOW DATABASES; */
+
+-- create databases
+/* CREATE DATABASE shop_store; */
+
+-- drop databases
+/* DROP DATABASE shop_store; */
+
+-- create database
+/* CREATE DATABASE shop_store; */
+
+-- use database
+/* USE shop_store; */
+
+-- select database
+/* SELECT DATABASE(); */
+
+-- create table
+/* CREATE TABLE chat_app(
+    address VARCHAR(20),
+    phone_no INT
+) */
+
+-- show tables
+/* SHOW TABLES; */
+
+-- show coloumn of tables
+/* SHOW COLUMNS FROM chat_app; */
+/* DESC chat_app; */
+
+-- drop table
+/* DROP TABLE chat_app; */
